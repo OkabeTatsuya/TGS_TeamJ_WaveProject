4c569d82a6f466da8fdab8efbc67a7fa6d9f2ebb
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-02-27 14:32:09 +0900

    [Add] 作業環境の作成

---
+++ ProjectData/GameSources/Character.h
@@ -0,0 +1,18 @@
+/*!
+@file Character.h
+@brief キャラクターなど
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross{
+	class player {
+		int a;
+
+	public :
+		void Scene() {
+		}
+	};
+}
+//end basecross
