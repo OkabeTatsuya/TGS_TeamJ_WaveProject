7cb7dfde9383edae87914246e0d81ebaf34cbfe5
meijyo <zeal19300100@c-league.jp>
2020-03-16 10:26:14 +0900

    [Add]タイトルステージにBGMとSE、Bボタンによるシーン移動の追加

--- ProjectData/GameSources/TitleStage.h
+++ ProjectData/GameSources/TitleStage.h
@@ -1,13 +1,27 @@
 #pragma once
 #include "stdafx.h"
 
+//コントローラー
 namespace basecross {
+	template<typename T>
+	struct InputHandler {
+		void PushHandle(const shared_ptr<T>& Obj) {
+			auto cutlVec = App::GetApp()->GetInputDevice().GetControlerVec();
+			if (cntlVec[0].bconnected) {
+			}
+		}
+	};
 
 	//--------------------------------------------------------------------------------------
 	//	ゲームステージクラス
 	//--------------------------------------------------------------------------------------
 
 	class TitleStage : public Stage {
+		//BGM
+		shared_ptr<SoundItem>m_BGM;
+		//SE
+		shared_ptr<SoundItem>m_SE;
+
 		//ビューの作成
 		void CreateViewLight();
 	public:
@@ -18,6 +32,10 @@ namespace basecross {
 
 		//初期化
 		virtual void OnCreate()override;
+
+		virtual void OnDestroy()override;
+
+		virtual void OnUpdate()override;
 	};
 }
 //end basecross
