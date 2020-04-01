b69a09ae15238a08a18d763d51f0e55d5529f545
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-04-01 10:00:01 +0900

    [Change]　OnCreateで作成するオブジェクト

--- ProjectData/GameSources/GameStage.cpp
+++ ProjectData/GameSources/GameStage.cpp
@@ -36,17 +36,14 @@ namespace basecross {
 			CreateViewLight();
             SetPhysicsActive(true);
 
-			//AddGameObject<WaveSpawner>();
+			AddGameObject<BGGenerator>();
 
             AddGameObject<Player>(Vec3(0, 0, 0), Vec3(1, 1, 1),Vec3(-4.0, 2, 0));
             AddGameObject<GroundCollision>(Vec3(0, 0, 0), Vec3(1, 0.5, 1), Vec3(-4, -2, 0));
+			AddGameObject<WaveSpawner>();
 
             AddGameObject<ScoreUIPanel>(Vec3(0.0f), Vec3(50.0f, 50.0f, 1.0f), Vec2(600.0f, 350.f), float(2.0f),L"",7);
 
-			AddGameObject<Wave>(Vec3(0, 0, 0), Vec3(1, 1, 1), Vec3(1, -1.5, 0));
-			AddGameObject<Wave>(Vec3(0, 0, 0), Vec3(1, 1, 1), Vec3(9, -1.5, 0));
-			AddGameObject<Wave>(Vec3(0, 0, 0), Vec3(1, 1, 1), Vec3(17, -1.5, 0));
-			
 			auto BGM = App::GetApp()->GetXAudio2Manager();
 			m_BGM = BGM->Start(L"SampleBGM.wav", XAUDIO2_LOOP_INFINITE, 0.5f);
 		}
