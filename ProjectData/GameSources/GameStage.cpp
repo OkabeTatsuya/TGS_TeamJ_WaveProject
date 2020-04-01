2d65b05c6663f965349fc5be77aa59271977dbd6
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-04-01 12:02:32 +0900

    [Change]　オブジェクトのレイヤーを修正

--- ProjectData/GameSources/GameStage.cpp
+++ ProjectData/GameSources/GameStage.cpp
@@ -39,15 +39,11 @@ namespace basecross {
 			AddGameObject<BGGenerator>();
 
 			AddGameObject<WaveSpawner>();
-            AddGameObject<Player>(Vec3(0, 0, 0), Vec3(1, 1, 1),Vec3(-4.0, 0, 0));
-            AddGameObject<SeaCollision>(Vec3(0, 0, 0), Vec3(1, 0.5, 1), Vec3(-4, -2, 0));
-            AddGameObject<Sea>(Vec3(0, 0, 0), Vec3(11, 4, 1), Vec3(0, -3.7, 0));
+            AddGameObject<Player>(Vec3(0, 0, 0), Vec3(1, 1, 1),Vec3(-4.0, 0, -3.0));
+            AddGameObject<SeaCollision>(Vec3(0, 0, 0), Vec3(1, 0.5, 1), Vec3(-4, -2, -3.0));
+            AddGameObject<Sea>(Vec3(0, 0, 0), Vec3(11, 4, 1), Vec3(0, -3.7, -5.0));
 
             AddGameObject<ScoreUIPanel>(Vec3(0.0f), Vec3(50.0f, 50.0f, 1.0f), Vec2(600.0f, 350.f), float(2.0f),L"",7);
-
-			AddGameObject<Wave>(Vec3(0, 0, 0), Vec3(1, 1, 1), Vec3(-1, -1.5, 0));
-			AddGameObject<Wave>(Vec3(0, 0, 0), Vec3(1, 1, 1), Vec3(9, -1.5, 0));
-			AddGameObject<Wave>(Vec3(0, 0, 0), Vec3(1, 1, 1), Vec3(17, -1.5, 0));
 
 			auto BGM = App::GetApp()->GetXAudio2Manager();
 			m_BGM = BGM->Start(L"SampleBGM.wav", XAUDIO2_LOOP_INFINITE, 0.5f);
