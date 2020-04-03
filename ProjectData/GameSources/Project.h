a7963921d7bce3965d3423a9f560240254c45db3
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-04-03 16:18:50 +0900

    [Change]　ヘッダーファイルを追加

--- ProjectData/GameSources/Project.h
+++ ProjectData/GameSources/Project.h
@@ -23,9 +23,13 @@
 #include "Wave.h"
 #include "WaveCollision.h"
 #include "MoveBG.h"
+#include "GoalObject.h"
+
 #include "SpawnerBase.h"
 #include "WaveSpawner.h"
 #include "BGGenerator.h"
+#include "GoalGenerator.h"
+
 #include "ImageUI.h"
 #include "FlashingUI.h"
 
