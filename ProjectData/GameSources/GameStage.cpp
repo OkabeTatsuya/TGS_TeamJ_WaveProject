64aabb9d37ff0e84ec617eb8fba3a5f2747a1c9c
sa_tatuya <sa_tatuya0101@yahoo.co.jp>
2020-04-13 11:31:44 +0900

    [Change]プレイヤーの大きさ変更

--- ProjectData/GameSources/GameStage.cpp
+++ ProjectData/GameSources/GameStage.cpp
@@ -51,7 +51,7 @@ namespace basecross {
 			AddGameObject<GoalGenerator>();
 
             AddGameObject<Sea>(Vec3(0.0f), Vec3(10.05f, 2.0f, 1.0f), Vec3(0.0, -4.1, -5.0));
-            AddGameObject<Player>(Vec3(0, 0, 0), Vec3(1, 1, 1),Vec3(-4.0, -2, -3.0));
+            AddGameObject<Player>(Vec3(0, 0, 0), Vec3(1.5f, 1.5f, 1),Vec3(-4.0, -2, -3.0));
             AddGameObject<SeaCollision>(Vec3(0, 0, 0), Vec3(1, 0.5, 1), Vec3(-4, -4, -3.0));
 
             AddGameObject<ScoreUIPanel>(Vec3(0.0f), Vec3(50.0f, 50.0f, 1.0f), Vec2(600.0f, 350.f), float(2.0f),L"",7);
