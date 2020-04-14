f8d76efde97ab33f4b14b464912f9cdcb556794d
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-04-14 14:09:30 +0900

    [Change]　ゴール生成オブジェクトの修正

--- ProjectData/GameSources/GoalGenerator.cpp
+++ ProjectData/GameSources/GoalGenerator.cpp
@@ -10,8 +10,13 @@ namespace basecross {
 	GoalGenerator::GoalGenerator(const shared_ptr<Stage>& StagePtr) :
 		SpawnerBase(StagePtr)
 	{
-		m_objState = { Vec3(0.0f), Vec3(1.0f, 10.0f, 1.0f), Vec3(-6.0f,0.0f,-3.0f), L"BGImage.png" };
-		m_offsetPos = 6.0f;
+		m_objState1 = { Vec3(0.0f), Vec3(10.0f, 10.0f, 1.0f), Vec3(-6.0f,-2.0f,-3.0f), L"Goal1.png" };
+		m_objState2 = { Vec3(0.0f), Vec3(10.0f, 10.0f, 1.0f), Vec3(-6.0f,-2.0f,-3.0f), L"Goal2.png" };
+
+		m_offsetPos = 7.5f;
+
+		m_spawnTimer = 0.0f;
+		m_maxSpawnTime = 2.0f;
 	}
 
 	void GoalGenerator::OnCreate() {
@@ -23,28 +28,58 @@ namespace basecross {
 	}
 
 	void GoalGenerator::CreateObject() {
-			m_goalObject = GetStage()->AddGameObject<GoalObject>(m_objState.Rot, m_objState.Scr, m_objState.Pos);
+		m_goalObject.push_back(GetStage()->AddGameObject<GoalObject>(m_objState1.Rot, m_objState1.Scr, m_objState1.Pos, m_objState1.Tex));
+		m_goalObject.push_back(GetStage()->AddGameObject<GoalObject>(m_objState2.Rot, m_objState2.Scr, m_objState2.Pos, m_objState2.Tex));
 	}
 
 	void GoalGenerator::GeneratGoal() {
-		if (GameManager::GetInstance().GetIsStopSpawner() && !m_isStopSpawn) {
-			VisibleGoal();			
+		Bit_SpawnFlag spawnFlag;
+		auto gameStage = App::GetApp()->GetScene<Scene>()->GetActiveTypeStage<GameStage>();
+
+		if (!m_isStopSpawn) {
+			auto waveFlag = gameStage->ConfirmSpawnFlag(spawnFlag.wave);
+			auto ItemFlag = gameStage->ConfirmSpawnFlag(spawnFlag.item);
+			auto stopSpawner = waveFlag && (ItemFlag && true) ? true : false;
+
+			if (stopSpawner) {
+				auto delter = App::GetApp()->GetElapsedTime();
+				m_spawnTimer += delter;
+				//VisibleGoal();
+			}
+
+			if (m_maxSpawnTime < m_spawnTimer) {
+				VisibleGoal();
+			}
 		}
 	}
 
 	//オブジェクトを表示する
 	void GoalGenerator::VisibleGoal() {
-		if (!m_goalObject->GetIsMove()) {
-			m_goalObject->GetComponent<Transform>()->SetPosition(Vec3(m_offsetPos, m_objState.Pos.y, m_objState.Pos.z));
-			m_goalObject->SetIsMove(true);
-			m_spawnCount++;
-			m_isStopSpawn = true;
+		for (int i = 0; i < m_goalObject.size(); i++) {
+			if (!m_goalObject[i]->GetIsMove()) {
+				m_goalObject[i]->GetComponent<Transform>()->SetPosition(Vec3(m_offsetPos, m_objState1.Pos.y, m_objState1.Pos.z));
+				m_goalObject[i]->SetIsMove(true);
+				//m_isStopSpawn = true;
+			}
+			else {
+				m_goalObject[i]->GetComponent<Transform>()->SetPosition(Vec3(m_offsetPos, m_objState1.Pos.y, m_objState1.Pos.z));
+				m_goalObject[i]->SetIsMove(true);
+				//m_isStopSpawn = true;
+			}
 		}
+		m_isStopSpawn = true;
 	}
 
 	void GoalGenerator::EndGeneratGoal() {
 		auto gameEndFlag = GameManager::GetInstance().GetIsGameEnd();
-		if (gameEndFlag)
+
+		if (gameEndFlag && !m_isStopSpawn) {
+			auto delter = App::GetApp()->GetElapsedTime();
+			m_spawnTimer += delter;
+		}
+
+		if (m_maxSpawnTime < m_spawnTimer) {
 			m_isStopSpawn = true;
+		}
 	}
 }
--- ProjectData/GameSources/GoalGenerator.h
+++ ProjectData/GameSources/GoalGenerator.h
@@ -8,17 +8,19 @@
 
 namespace basecross {
 	class GoalGenerator : public SpawnerBase {
-		ObjectState m_objState;
-		
-		shared_ptr<GoalObject> m_goalObject;
+		ObjectState m_objState1;
+		ObjectState m_objState2;
+
+		vector<shared_ptr<GoalObject>> m_goalObject;
 		vector<float> m_createPos;
 
 		Vec3 m_spawnPos;
 
 		float m_offsetPos;
-		int m_spawnCount;
-		float m_spawnTimer = 0.0f;
 		bool m_isStopSpawn;
+
+		float m_spawnTimer;
+		float m_maxSpawnTime;
 
 	public:
 		GoalGenerator(const shared_ptr<Stage>& StagePtr);
