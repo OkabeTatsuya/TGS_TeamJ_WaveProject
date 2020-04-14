2cd1ea44da27f4c209320e32bf66b6453c5e6354
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-04-14 14:26:28 +0900

    [Change]　波の画像変更

--- ProjectData/GameSources/Wave.cpp
+++ ProjectData/GameSources/Wave.cpp
@@ -21,7 +21,7 @@ namespace basecross {
     }
 
     void Wave::OnCreate() {
-        DrawingImage(L"SamllWave.png");
+        DrawingImage(L"SamllWave2ver.png");
         auto transPtr = AddComponent<Transform>();
         transPtr->SetPosition(m_position);
         transPtr->SetScale(m_scale);
