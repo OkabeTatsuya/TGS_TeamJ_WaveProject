5590c9964a179f288c55d69bef531b8219ed7758
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-04-20 10:04:31 +0900

    [Change]　選択されたステージを保存する場所を追加

--- ProjectData/GameSources/GameManager.h
+++ ProjectData/GameSources/GameManager.h
@@ -20,6 +20,7 @@ namespace basecross {
 			m_gameScore = 0;
 			m_baseJumpScore = 100;
 			m_baseActionScore = 200;
+			m_selectStageNum = 0;
 
 			m_isFirstStop = true;
 			m_isStopSpawner = false;
@@ -39,15 +40,13 @@ namespace basecross {
 		int m_baseJumpScore;
 		int m_baseActionScore;
 
-
 		float m_gameSpeed;
 
-		//ゲームがスタートした時の硬直時間
-		float m_startTimeCount;
-		float m_maxStartTime;
 
 		float m_saveNum;
-		float m_stageNum;
+
+		//選ばれたステージの番号
+		int m_selectStageNum;
 
 		bool m_isFirstStop;
 		bool m_isStopSpawner;
@@ -64,7 +63,7 @@ namespace basecross {
 		float GetGameSpeed() { return m_gameSpeed; }
 		bool GetIsStopSpawner() { return m_isStopSpawner; }
 		bool GetIsGameEnd() { return m_isGameEnd; }
-
+		int GetSelectStageNum() { return m_selectStageNum; }
 
 		//セッター
 		void SetIsGameEnd(bool set) { m_isGameEnd = set; }
@@ -72,6 +71,7 @@ namespace basecross {
 		void SetIsStopSpawner(bool set) { m_isStopSpawner = set; }
         void SetScoreUIPanel(shared_ptr<ScoreUIPanel> set) { m_scoreUIPanel = set; }
 		void ReSetGameScore() { m_gameScore = 0; }
+		void SetSelectStageNum(int set) { m_selectStageNum = set; }
 
 		//ジャンプのスコア加算処理
 		///magnification 倍率、　combo 続けてジャンプした回数
