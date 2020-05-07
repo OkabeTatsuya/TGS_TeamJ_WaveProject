279e1ea7335ec11926335c3ba473ca8c90a46bc7
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-05-07 11:41:12 +0900

    [Change]　読み込むCSVデータを選択できるように変更

--- ProjectData/GameSources/SpawnerBase.cpp
+++ ProjectData/GameSources/SpawnerBase.cpp
@@ -16,12 +16,20 @@ namespace basecross {
 
 	}
 
-	void SpawnerBase::LoadCSV() {
+	void SpawnerBase::LoadCSV(en_ObjectType objType) {
 		int SelectStage = GameManager::GetInstance().GetSelectStageNum();
 
 		wstring mediaDir;
 		App::GetApp()->GetDataDirectory(mediaDir);
-		auto csvFilePths = mediaDir + L"CSV/" + m_strFileNameCSV.stageStr[SelectStage] + L".csv";
+		wstring csvFilePths;
+
+		if (objType == en_ObjectType::en_Wave) {
+			csvFilePths = mediaDir + L"CSV/" + m_strFileNameCSV.stageStr[SelectStage] + L".csv";
+		}
+		else if (objType == en_ObjectType::en_Item) {
+			csvFilePths = mediaDir + L"CSV/" + m_strFileNameCSV.itemStr[SelectStage] + L".csv";
+		}
+
 		CsvFile LoadCsvFile;
 		m_gameStageCs.SetFileName(csvFilePths);
 		m_gameStageCs.ReadCsv();
@@ -29,12 +37,31 @@ namespace basecross {
 		for (size_t i = 0; i < lineVec.size(); i++) {
 			vector<wstring> tokens;
 			Util::WStrToTokenVector(tokens, lineVec[i], L',');
-			for (size_t j = 0; j < tokens.size(); j++) {
-				float num = stof(tokens[j]);
-				m_createPos.push_back(num);
-			}
+			ReadCSVLine(tokens, objType);
+			//for (size_t j = 0; j < tokens.size(); j++) {
+			//	float num = stof(tokens[j]);
+			//	m_createPosX.push_back(num);
+			//}
 		};
-
-		GetComponent<Transform>()->GetPosition();
+		//GetComponent<Transform>()->GetPosition();
 	}
+
+	void SpawnerBase::ReadCSVLine(vector<wstring> csvLine, en_ObjectType objType) {
+		if (objType == en_ObjectType::en_Wave) {
+			for (size_t j = 0; j < csvLine.size(); j++) {
+				float num = stof(csvLine[j]);
+				m_createPosX.push_back(num);
+			}
+		}
+		else if (objType == en_ObjectType::en_Item) {
+			for (size_t j = 0; j < csvLine.size(); j++) {
+				float posX = stof(csvLine[j]);
+				float posY = stof(csvLine[j + 1]);
+				m_createPosX.push_back(posX);
+				m_createPosY.push_back(posY);
+				j++;
+			}
+		}
+	}
+
 }
