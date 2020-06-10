b08df350f49ab2ce4e7d594ab1e96d3d60030a04
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-06-10 09:25:19 +0900

    [Change]　ウィンドウに表示するタイトルを変更

--- ProjectData/GameSources/WinMain.cpp
+++ ProjectData/GameSources/WinMain.cpp
@@ -10,7 +10,7 @@ LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 
 //íËêî
 const wchar_t* pClassName = L"BaseCrossFullClass";
-const wchar_t* pWndTitle = L"îgÇ…èÊÇÍ";
+const wchar_t* pWndTitle = L"Ç»Ç›Ç…ÇÃÇÍ";
 
 
 //--------------------------------------------------------------------------------------
